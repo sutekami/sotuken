790a2f2c6a0c133bcbaf12105d29fa8c0e897f01
MashuGiga/sutekami <gigamasi@gmail.com>
2024-04-23 22:54:29 +0900

    mysql導入、prisma導入

---
+++ svelte/src/routes/login/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  let name = 1, email = 2, password = 3;
+
+  const handleSave = (event) => {
+    console.log(event)
+  }
+</script>
+
+<div>login</div>
+<input type="text" placeholder="name" bind:value={name}/>
+<input type="text" placeholder="email" bind:value={email}/>
+<input type="text" placeholder="password" bind:value={password}/>
+<button on:click={handleSave}>submit</button>
+
+<style></style>
