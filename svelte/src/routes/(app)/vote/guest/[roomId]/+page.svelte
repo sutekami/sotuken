5174b729958c8eedde699b079a782f0452690850
sutekami <gigamasi@gmail.com>
2024-11-09 21:14:30 +0900

    add: secureをfalseに

--- svelte/src/routes/(app)/vote/guest/[roomId]/+page.svelte
+++ svelte/src/routes/(app)/vote/guest/[roomId]/+page.svelte
@@ -10,6 +10,7 @@
 
   const socket = io(`http://${data.DOMAIN_NAME}:${data.SERVER_PORT}`, {
     withCredentials: true,
+    secure: false,
   });
   let guestName: string;
   let inVoting: boolean;
